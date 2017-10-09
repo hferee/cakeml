fd650b0197380956e86276cb40db99d74e59dd41
hferee <hferee@users.noreply.github.com>
2017-10-09 15:42:02 +0100

    Update cached-deps.sh

--- developers/cached-deps.sh
+++ developers/cached-deps.sh
@@ -7,6 +7,7 @@ set -e
 pushd $HOME
 git clone https://github.com/HOL-Theorem-Prover/HOL.git
 cd HOL
+echo "val polymllibdir = "/usr/lib/x86_64-linux-gnu/";" > tools-poly/poly-includes.ML 
 poly < tools-poly/smart-configure.sml
 ./bin/build
 
