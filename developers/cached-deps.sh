200588e96f3df2143f1ecd7e26cae73b5789a89e
hferee <hferee@users.noreply.github.com>
2017-10-11 13:25:33 +0100

    Update cached-deps.sh

--- developers/cached-deps.sh
+++ developers/cached-deps.sh
@@ -3,9 +3,11 @@
 ## probably obsolete, since Travis is no longer supported.
 
 set -e
-
 pushd $HOME
-git clone https://github.com/HOL-Theorem-Prover/HOL.git
+
+if [ ! -d "HOL" ] ; then
+    git clone "https://github.com/HOL-Theorem-Prover/HOL.git" "HOL"
+fi
 cd HOL
 git pull
 ./bin/build --nograph || (
