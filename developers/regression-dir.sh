1875632b86191b6c1eb604a88eccc4484e89d1ad
hferee <hferee@users.noreply.github.com>
2017-10-11 11:51:06 +0100

    Add files via upload

---
+++ developers/regression-dir.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")
+source misc.sh
+cd ..
+
+case $(uname -a) in
+  Linux* ) TIMECMD="/usr/bin/time -o timing.log -f '%U %M'";;
+esac
+
+echo
+
+#save the current state
+if [ ! -d $1 ]
+then
+  echo "Ignoring non-existent directory $1"
+  exit 0
+fi
+pushd $1 > /dev/null 2>&1
+/bin/rm -f timing.log 2> /dev/null
+Holmake cleanAll &&
+if eval $TIMECMD Holmake > regression.log 2>&1
+then
+  echo -n "OK: $1"
+  if [ -f timing.log ]
+  then
+    printf '%0.*s' $((36 - ${#1})) "$pad"
+    eval displayline $(cat timing.log)
+  else
+      echo
+  fi
+  if [ -x selftest.exe ]
+  then
+    if ./selftest.exe >> regression.log 2>&1
+    then
+      echo "OK: $1 (selftest)"
+    else
+      echo "FAILED: $1 (selftest)"
+      cat "$1/regression.log"
+      exit 1
+    fi
+  fi
+else
+  echo "FAILED: $1"
+  exit 1
+fi
+popd > /dev/null 2>&1
